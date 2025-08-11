903b725d20b86788164c54a14e38e22ccf8d0a92
John Larsen <johninen@gmail.com>
2025-08-11 21:44:23 +0200

    Stan. And Pint.

--- phpstan/Blueprint.php
+++ phpstan/Blueprint.php
@@ -5,14 +5,14 @@ namespace Illuminate\Database\Schema;
 class Blueprint
 {
     // Externally called macros in your migrations
-    public function vectorLite(string $column, $length = null, $fixed = false): void {}
+    public function vectorLite(string $column, ?int $length = null, bool $fixed = false): void {}
 
-    public function vectorLiteCluster(string $column, $length = null, $fixed = false): void {}
+    public function vectorLiteCluster(string $column, ?int $length = null, bool $fixed = false): void {}
 
     public function dropVectorLite(string $column): void {}
 
     public function dropVectorLiteColumns(string $column): void {}
 
     // Internal helper macro you call from other macros
-    public function vectorLiteColumns(string $column, $length = null, $fixed = false): void {}
+    public function vectorLiteColumns(string $column, ?int $length = null, bool $fixed = false): void {}
 }
