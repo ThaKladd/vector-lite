017320d0cf270605f7bd8ffd509a5af5ac536abe
John Larsen <johninen@gmail.com>
2025-08-13 21:00:12 +0200

    Change db name in test.

--- tests/VectorLiteTest.php
+++ tests/VectorLiteTest.php
@@ -75,7 +75,7 @@ it('can create vector table and use vector methods - mysql', function () {
     $this->setUpDatabase();
 
     // Now run assertions against MySQL
-    expect(DB::connection()->getDatabaseName())->toBe('db');
+    expect(DB::connection()->getDatabaseName())->toBe('test_db');
     $vectorAmount = 10;
 
     // Check if the table exists
